7aede038539b4c8144638baffbc8beba3fb5bddd
camryn_walter <camryn.walter@ufl.edu>
2025-11-16 17:05:49 -0500

    completed my parts

--- calculator.py
+++ calculator.py
@@ -1,11 +1,49 @@
+# https://github.com/camry-walter/lab11-CW-AE.git
+# Partner 1: Camryn Walter
+# Partner 2: Adrian Espinoza
+
+#user.email"camryn.walter@ufl.edu git add calculator.py	"
+#user.name"Camryn Walter"
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
+
 # First example
-def add(a, b): 
-    pass
+import math
+def square_root(a):
+    if a > 0:
+        return math.sqrt(a)
+    else:
+        raise ValueError
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    try:
+        return b/a
+    except ZeroDivisionError:
+        raise ZeroDivisionError
+
+def logarithm(a, b):
+    try:
+        return math.log(a, b)
+    except ValueError:
+        raise ValueError
+
+def exponent(a, b):
+    return a**b
 
 
