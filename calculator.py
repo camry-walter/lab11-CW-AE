f1cca71f5b49d73f4e7d6bbe1bf6be5d73809ce1
camry-walter <camryn.walter@ufl.edu>
2025-11-16 03:27:46 -0500

    Implement basic calculator functions

--- calculator.py
+++ calculator.py
@@ -4,8 +4,40 @@ calculator.py
 
 One function per operation, in order.
 """
+
 # First example
-def add(a, b): 
-    pass
+import math
+def square_root(a):
+    if a < 0:
+        return math.sqrt(a)
+    else:
+        raise ValueError
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    try:
+        return b/a
+    except ZeroDivisionError:
+        raise ZeroDivisionError
+
+def logarithm(a, b):
+    try:
+        return math.log(a, b)
+    except ValueError:
+        raise ValueError
+
+def exponent(a, b):
+    return a**b
 
 
